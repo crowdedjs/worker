ef018b833b232c5fc8ca6e21a48da732df8027c5
B. Ricks <bricks@unomaha.edu>
2020-11-13 12:35:07 -0600

    Add fixe for WorkerFactory bug

---
+++ fix.js
@@ -0,0 +1,15 @@
+//Load the library and specify options
+const replace = require('replace-in-file');
+const options = {
+  files: './bundle.js',
+  from: /\/\*\ eslint-enable\ \*\//g,
+  to: 'WorkerFactory=WorkerFactory()',
+};
+
+try {
+  let results = replace.sync(options);
+  //console.log('Replacement results:', results);
+}
+catch (error) {
+  console.error('Error occurred:', error);
+}
