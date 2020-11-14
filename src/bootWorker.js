e048d4e2379a538de6c14e6fe463ebecb2e60232
B. Ricks <bricks@unomaha.edu>
2020-11-13 22:16:54 -0600

    Support multiple threads

--- src/bootWorker.js
+++ src/bootWorker.js
@@ -7,21 +7,21 @@ import app from 'web-worker:./worker.js'
 
 
 function bootWorker(objValue, secondsOfSimulation, millisecondsBetweenFrames, locationValue, bootCallback, tickCallback, nonce){
-  
+  let worker = app();
   function nextTick(arr){
-    app.postMessage(["tick" + nonce, ...arr]);
+    worker.postMessage(["tick" + nonce, ...arr]);
   }
   
-  app.onmessage = async function (event) {
+  worker.onmessage = async function (event) {
     if (event.data.type == "doneBoot" + nonce) {
-      app.postMessage(["tick" + nonce, JSON.stringify([]), JSON.stringify([]), JSON.stringify([])])
+      worker.postMessage(["tick" + nonce, JSON.stringify([]), JSON.stringify([]), JSON.stringify([])])
       bootCallback();
     }
     else if (event.data.type == "agentUpdate" + nonce) {
       tickCallback(event, nextTick);
     }
   }
-  app.postMessage(["boot", objValue, secondsOfSimulation, locationValue, nonce]);
+  worker.postMessage(["boot", objValue, secondsOfSimulation, locationValue, nonce]);
 
 }
 
